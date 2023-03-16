b1fb75b3ded46c599188806bbdb9be32ba86182a
Mehdi <80045406+mehdij87@users.noreply.github.com>
2023-03-15 21:17:18 -0400

    Build blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
